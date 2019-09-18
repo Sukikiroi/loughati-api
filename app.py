84e46d16f757468843288efcf14b715124ddb696
serinir <imadom568@gmai.com>
2019-09-18 23:06:58 +0100

    users data base added api register and login next

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from loughati.resources.dico import Dico
 from loughati.resources.feedBack import FeedBack
 from loughati.resources.profile import Profile
 from loughati.resources.activity import Activity
+from loughati.resources.register import Register
+from loughati import app
 
-app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Dico, '/dico')
 api.add_resource(FeedBack, '/feedback')
 api.add_resource(Profile, '/profile')
 api.add_resource(Activity, '/activity')
-
+api.add_resource(Register,'/register')
 
 if __name__ == '__main__':
     app.run(debug=True)
